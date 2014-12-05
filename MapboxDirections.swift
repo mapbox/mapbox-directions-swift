5f8c652e575cffc25997fa962f948c4500633662
Justin R. Miller <incanus@codesorcery.net>
2014-12-04 18:27:35 -0800

    Point, optionals, Mapbox properties, and init with JSON

--- MapboxDirections.swift
+++ MapboxDirections.swift
@@ -5,6 +5,13 @@ public typealias MBDirectionsHandler = (MBDirectionsResponse!, NSError!) -> Void
 
 //public typealias MBETAHandler = (MBETAResponse!, NSError!) -> Void
 
+public struct MBPoint {
+
+    public var name: String
+    public var coordinate: CLLocationCoordinate2D
+
+}
+
 public class MBETAResponse {
 
     private(set) var sourceCoordinate: CLLocationCoordinate2D
@@ -21,15 +28,61 @@ public class MBETAResponse {
 
 public class MBRouteStep {
 
+    enum Direction: String {
+        case N = "N"
+        case NE = "NE"
+        case E = "E"
+        case SE = "SE"
+        case S = "S"
+        case SW = "SW"
+        case W = "W"
+        case NW = "NW"
+    }
+
+    enum ManeuverType: String {
+        case Continue = "continue"
+        case BearRight = "bear right"
+        case TurnRight = "turn right"
+        case SharpRight = "sharp right"
+        case UTurn = "u-turn"
+        case SharpLeft = "sharp left"
+        case TurnLeft = "turn left"
+        case BearLeft = "bear left"
+        case Waypoint = "waypoint"
+        case Depart = "depart"
+        case EnterRoundabout = "enter roundabout"
+        case Arrive = "arrive"
+    }
+
 //    var polyline: MKPolyline! { get }
     private(set) var instructions: String
 //    var notice: String! { get }
-    private(set) var distance: CLLocationDistance
+    private(set) var distance: CLLocationDistance?
 //    var transportType: MKDirectionsTransportType { get }
 
-    internal init(instructions: String, distance: CLLocationDistance) {
-        self.instructions = instructions
-        self.distance = distance
+    // Mapbox-specific stuff
+    private(set) var duration: NSTimeInterval?
+    private(set) var way_name: String?
+    private(set) var direction: Direction?
+    private(set) var heading: CLLocationDegrees?
+    private(set) var maneuverType: ManeuverType
+    private(set) var maneuverLocation: CLLocationCoordinate2D
+
+    internal init(json: JSON) {
+        self.instructions = json["maneuver"]["instruction"].stringValue
+        self.distance = json["distance"].double
+        self.duration = json["duration"].double
+        self.way_name = json["way_name"].string
+        if let directionString = json["direction"].string? {
+            self.direction = Direction(rawValue: json["direction"].stringValue)
+        }
+        self.heading = json["heading"].double
+        self.maneuverType = ManeuverType(rawValue: json["maneuver"]["type"].stringValue)!
+        self.maneuverLocation = {
+            let coordinates = json["maneuver"]["location"]["coordinates"].arrayValue
+            let location = CLLocationCoordinate2D(latitude: coordinates[1].doubleValue, longitude: coordinates[0].doubleValue)
+            return location
+            }()
     }
 
 }
@@ -44,10 +97,33 @@ public class MBRoute {
     private(set) var expectedTravelTime: NSTimeInterval
 //    var transportType: MKDirectionsTransportType { get }
 
-    internal init(steps: [MBRouteStep], distance: CLLocationDistance, expectedTravelTime: NSTimeInterval) {
-        self.steps = steps
-        self.distance = distance
-        self.expectedTravelTime = expectedTravelTime
+    // Mapbox-specific stuff
+    private(set) var summary: String
+    private(set) var geometry: [CLLocationCoordinate2D]
+
+    private(set) var origin: MBPoint
+    private(set) var destination: MBPoint
+
+    internal init(origin: MBPoint, destination: MBPoint, json: JSON) {
+        self.origin = origin
+        self.destination = destination
+        self.steps = {
+            var steps = [MBRouteStep]()
+            for step: JSON in json["steps"].arrayValue {
+                steps.append(MBRouteStep(json: step))
+            }
+            return steps
+            }()
+        self.distance = json["distance"].doubleValue
+        self.expectedTravelTime = json["duration"].doubleValue
+        self.summary = json["summary"].stringValue
+        self.geometry = {
+            var points = [CLLocationCoordinate2D]()
+            for point: JSON in json["geometry"]["coordinates"].arrayValue {
+                points.append(CLLocationCoordinate2D(latitude: point.arrayValue[1].doubleValue, longitude: point.arrayValue[0].doubleValue))
+            }
+            return points
+            }()
     }
 
 }
@@ -118,11 +194,19 @@ public class MBDirections: NSObject, NSURLConnectionDelegate, NSURLConnectionDat
                 var parsedRoutes = [MBRoute]()
                 let json = JSON(data: data)
                 for route: JSON in json["routes"].arrayValue {
-                    var parsedSteps = [MBRouteStep]()
-                    for step: JSON in route["steps"].arrayValue {
-                        parsedSteps.append(MBRouteStep(instructions: step["maneuver"]["instruction"].stringValue, distance: step["distance"].doubleValue))
-                    }
-                    parsedRoutes.append(MBRoute(steps: parsedSteps, distance: route["distance"].doubleValue, expectedTravelTime: route["duration"].doubleValue))
+                    let origin = MBPoint(name: json["origin"]["properties"]["name"].stringValue,
+                        coordinate: {
+                            let coordinates = json["origin"]["geometry"]["coordinates"].arrayValue
+                            return CLLocationCoordinate2D(latitude: coordinates[1].doubleValue,
+                                longitude: coordinates[0].doubleValue)
+                            }())
+                    let destination = MBPoint(name: json["destination"]["properties"]["name"].stringValue,
+                        coordinate: {
+                            let coordinates = json["destination"]["geometry"]["coordinates"].arrayValue
+                            return CLLocationCoordinate2D(latitude: coordinates[1].doubleValue,
+                                longitude: coordinates[0].doubleValue)
+                            }())
+                    parsedRoutes.append(MBRoute(origin: origin, destination: destination, json: route))
                 }
                 handler(MBDirectionsResponse(sourceCoordinate: self.request.sourceCoordinate, destinationCoordinate: self.request.destinationCoordinate, routes: parsedRoutes), nil)
             } else {
