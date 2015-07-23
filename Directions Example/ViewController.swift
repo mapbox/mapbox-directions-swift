0daa44ee9157f42ef819dc76ee4474170dced2c0
Justin R. Miller <incanus@codesorcery.net>
2015-07-22 17:42:05 -0700

    add view to avoid confusion

--- Directions Example/ViewController.swift
+++ Directions Example/ViewController.swift
@@ -8,6 +8,18 @@ class ViewController: UIViewController {
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
 
+        view.addSubview({ [unowned self] in
+            let label = UILabel(frame: CGRect(x: (self.view.bounds.size.width - 200) / 2,
+                y: (self.view.bounds.size.height - 40) / 2,
+                width: 200,
+                height: 40))
+            label.autoresizingMask = .FlexibleLeftMargin | .FlexibleRightMargin | .FlexibleTopMargin | .FlexibleBottomMargin
+            label.textColor = UIColor.whiteColor()
+            label.textAlignment = .Center
+            label.text = "Check the console"
+            return label
+            }())
+
         let mb = CLLocationCoordinate2D(latitude: 38.9131752, longitude: -77.0324047)
         let wh = CLLocationCoordinate2D(latitude: 38.8977, longitude: -77.0365)
 
