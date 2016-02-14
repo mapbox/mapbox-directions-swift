53615d49bc90c1fcddac4bbb055979f8bc2e225f
Minh Nguyễn <mxn@1ec5.org>
2016-02-13 18:53:16 -0800

    Revert "Eliminated some implicitly unwrapped optionals"
    This reverts commit e9c13aea692abf2ddf434f41cd8ca4b5ce0044a8, which is only valid starting in Swift 2.2.

--- MapboxDirections.swift
+++ MapboxDirections.swift
@@ -67,18 +67,18 @@ public class MBRouteStep {
     }
 
     //    var polyline: MKPolyline! { get }
-    internal(set) public var instructions: String
+    internal(set) public var instructions: String! = ""
     //    var notice: String! { get }
-    internal(set) public var distance: CLLocationDistance
+    internal(set) public var distance: CLLocationDistance = 0
     //    var transportType: MKDirectionsTransportType { get }
 
     // Mapbox-specific stuff
-    internal(set) public var duration: NSTimeInterval
-    internal(set) public var way_name: String
+    internal(set) public var duration: NSTimeInterval?
+    internal(set) public var way_name: String?
     internal(set) public var direction: Direction?
-    internal(set) public var heading: CLLocationDirection
+    internal(set) public var heading: CLLocationDirection?
     internal(set) public var maneuverType: ManeuverType?
-    internal(set) public var maneuverLocation: CLLocationCoordinate2D
+    internal(set) public var maneuverLocation: CLLocationCoordinate2D?
 
     internal init?(json: JSON) {
         if let maneuver = json["maneuver"] as? JSON,
@@ -86,19 +86,19 @@ public class MBRouteStep {
           let distance = json["distance"] as? Double,
           let duration = json["duration"] as? Double,
           let way_name = json["way_name"] as? String,
-          let directionString = json["direction"] as? String,
+          let direction = json["direction"] as? String,
           let heading = json["heading"] as? Double,
-          let typeString = maneuver["type"] as? String,
+          let type = maneuver["type"] as? String,
           let location = maneuver["location"] as? JSON,
           let coordinates = location["coordinates"] as? [Double] {
             self.instructions = instruction
             self.distance = distance
             self.duration = duration
             self.way_name = way_name
-            direction = Direction(rawValue: directionString)
+            self.direction = Direction(rawValue: direction)
             self.heading = heading
-            maneuverType = ManeuverType(rawValue: typeString)!
-            maneuverLocation = CLLocationCoordinate2D(latitude: coordinates[1], longitude: coordinates[0])
+            self.maneuverType = ManeuverType(rawValue: type)!
+            self.maneuverLocation = CLLocationCoordinate2D(latitude: coordinates[1], longitude: coordinates[0])
         } else {
             return nil
         }
