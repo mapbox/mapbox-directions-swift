69c1088415e25a567b3c669fe36a557ca42f7445
Minh Nguyễn <mxn@1ec5.org>
2016-04-28 17:42:28 -0700

    Synthesize route leg summaries
    Worked around mapbox/api-directions#567 by concatenating the two top way names by cumulative length along the route, but only if a route leg summary is absent.

--- MapboxDirections/MBDirectionsResponse.swift
+++ MapboxDirections/MBDirectionsResponse.swift
@@ -87,12 +87,22 @@ public class MBRouteLeg {
         self.source = source
         self.destination = destination
         self.profileIdentifier = profileIdentifier
-        steps = (json["steps"] as? [JSON] ?? []).map {
-            MBRouteStep(json: $0, profileIdentifier: profileIdentifier, version: version)
+        let name = json["summary"] as? String
+        var stepNamesByDistance: [String: CLLocationDistance] = [:]
+        steps = (json["steps"] as? [JSON] ?? []).map { json in
+            let step = MBRouteStep(json: json, profileIdentifier: profileIdentifier, version: version)
+            // If no summary is provided for some reason, synthesize one out of the two names that make up the longest cumulative distance along the route.
+            if let name = name where !name.isEmpty {
+                if let stepName = step.name where !stepName.isEmpty {
+                    stepNamesByDistance[stepName] = (stepNamesByDistance[stepName] ?? 0) + step.distance
+                }
+            }
+            return step
         }
         distance = json["distance"] as! Double
         expectedTravelTime = json["duration"] as! Double
-        name = json["summary"] as! String
+        let longestNames = Array(stepNamesByDistance.sort { $0.1 > $1.1 }.prefix(2))
+        self.name = name ?? longestNames.map { $0.0 }.joinWithSeparator(" – ")
     }
 }
 
