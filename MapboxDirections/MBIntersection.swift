35097e7af3a4ccae9c63543fd78259f36e4dd1de
bsudekum <bobby@mapbox.com>
2016-10-19 09:18:59 -0700

    make optional

--- MapboxDirections/MBIntersection.swift
+++ MapboxDirections/MBIntersection.swift
@@ -8,14 +8,14 @@ public class Intersection: NSObject, NSSecureCoding {
      
      The index of the item in the headings array that corresponds to the road that the containing route step uses to approach the intersection.
     */
-    public let approachIndex: Int = -1
+    public var approachIndex: Int = -1
     
     /**
      Index into the bearings/entry array.
      
      The index of the item in the headings array that corresponds to the road that the containing route step uses to leave the intersection.
     */
-    public let outletIndex: Int = -1
+    public var outletIndex: Int = -1
     
     /**
      An array of booleans, corresponding in a 1:1 relationship to the headings.
@@ -46,7 +46,7 @@ public class Intersection: NSObject, NSSecureCoding {
     */
     public var usableLanes: Set<Lane>?
     
-    internal init(approachIndex: Int?, outletIndex: Int?, entry: [Bool], location: CLLocationCoordinate2D, headings: [CLLocationDirection], lanes: [Lane]?, usableLanes: Set<Lane>) {
+    internal init(approachIndex: Int, outletIndex: Int, entry: [Bool], location: CLLocationCoordinate2D, headings: [CLLocationDirection], lanes: [Lane]?, usableLanes: Set<Lane>) {
         self.approachIndex = approachIndex
         self.outletIndex = outletIndex
         self.entry = entry
@@ -57,8 +57,8 @@ public class Intersection: NSObject, NSSecureCoding {
     }
     
     internal convenience init(json: JSONDictionary) {
-        let approachIndex = json["in"] as? Int
-        let outletIndex = json["out"] as? Int
+        let approachIndex = json["in"] as? Int ?? -1
+        let outletIndex = json["out"] as? Int ?? -1
         let entry = json["entry"] as! [Bool]
         let coords = json["location"] as! [Double]
         let location = CLLocationCoordinate2D.init(geoJSON: coords)
@@ -67,7 +67,7 @@ public class Intersection: NSObject, NSSecureCoding {
         var lanes = [Lane]()
         var usableLanes = Set<Lane>()
         
-        for laneJSON in lanesJSON! {
+        for laneJSON in lanesJSON ?? [] {
             let lane = Lane(json: laneJSON)
             lanes.append(lane)
             if laneJSON["valid"] as! Bool {
@@ -79,8 +79,8 @@ public class Intersection: NSObject, NSSecureCoding {
     }
     
     public required init?(coder decoder: NSCoder) {
-        approachIndex = decoder.decodeObjectForKey("approachIndex") as? Int
-        outletIndex = decoder.decodeObjectForKey("outletIndex") as? Int
+        approachIndex = decoder.decodeObjectForKey("approachIndex") as! Int
+        outletIndex = decoder.decodeObjectForKey("outletIndex") as! Int
         entry = decoder.decodeObjectForKey("entry") as! [Bool]
         let coordinateDictionaries = decoder.decodeObjectForKey("location") as? [String: CLLocationDegrees]
         location = CLLocationCoordinate2D(latitude: coordinateDictionaries!["latitude"]!, longitude: coordinateDictionaries!["longitude"]!)
