3a7beb7bf6be9c3ddd1ec71ad27a2b0fb0341c9a
Minh Nguyễn <mxn@1ec5.org>
2016-10-29 14:01:29 -0700

    Beefed up watch example
    The watch example now exercises the most common directions code to ensure that it at least compiles.

--- WatchExample Extension/InterfaceController.swift
+++ WatchExample Extension/InterfaceController.swift
@@ -1,23 +1,49 @@
 import WatchKit
 import Foundation
+import MapboxDirections
 
+// A Mapbox access token is required to use the Directions API.
+// https://www.mapbox.com/help/create-api-access-token/
+let MapboxAccessToken = "<# your Mapbox access token #>"
 
 class InterfaceController: WKInterfaceController {
-
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-        // Configure interface objects here.
+        assert(MapboxAccessToken != "<# your Mapbox access token #>", "You must set `MapboxAccessToken` to your Mapbox access token.")
+        
+        let options = RouteOptions(waypoints: [
+            Waypoint(coordinate: CLLocationCoordinate2D(latitude: 38.9131752, longitude: -77.0324047), name: "Mapbox"),
+            Waypoint(coordinate: CLLocationCoordinate2D(latitude: 38.8977, longitude: -77.0365), name: "White House"),
+            ])
+        options.includesSteps = true
+        
+        Directions(accessToken: MapboxAccessToken).calculateDirections(options: options) { (waypoints, routes, error) in
+            guard error == nil else {
+                print("Error calculating directions: \(error!)")
+                return
+            }
+            
+            if let route = routes?.first, leg = route.legs.first {
+                print("Route via \(leg):")
+                
+                let distanceFormatter = NSLengthFormatter()
+                let formattedDistance = distanceFormatter.stringFromMeters(route.distance)
+                
+                let travelTimeFormatter = NSDateComponentsFormatter()
+                travelTimeFormatter.unitsStyle = .Short
+                let formattedTravelTime = travelTimeFormatter.stringFromTimeInterval(route.expectedTravelTime)
+                
+                print("Distance: \(formattedDistance); ETA: \(formattedTravelTime!)")
+                
+                for step in leg.steps {
+                    print("\(step.instructions)")
+                    if step.distance > 0 {
+                        let formattedDistance = distanceFormatter.stringFromMeters(step.distance)
+                        print("— \(formattedDistance) —")
+                    }
+                }
+            }
+        }
     }
-    
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-    
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-
 }
