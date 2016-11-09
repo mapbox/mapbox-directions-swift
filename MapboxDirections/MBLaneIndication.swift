2ac4b65330ffd83206eb021dd7d6afdb31cf29dc
Minh Nguyễn <mxn@1ec5.org>
2016-11-09 10:24:20 -0800

    Fixed roundtripping of right lane indication

--- MapboxDirections/MBLaneIndication.swift
+++ MapboxDirections/MBLaneIndication.swift
@@ -20,10 +20,10 @@ extension LaneIndication: CustomStringConvertible {
                 scope.insert(.StraightAhead)
             case "slight left":
                 scope.insert(.SlightLeft)
-            case "sharp left":
-                scope.insert(.SharpLeft)
             case "left":
                 scope.insert(.Left)
+            case "sharp left":
+                scope.insert(.SharpLeft)
             case "uturn":
                 scope.insert(.UTurn)
             case "none":
@@ -40,8 +40,8 @@ extension LaneIndication: CustomStringConvertible {
         if contains(LaneIndication.SharpRight) {
             descriptions.append("sharp right")
         }
-        if contains(LaneIndication.SharpRight) {
-            descriptions.append("sharp right")
+        if contains(LaneIndication.Right) {
+            descriptions.append("right")
         }
         if contains(LaneIndication.SlightRight) {
             descriptions.append("slight right")
