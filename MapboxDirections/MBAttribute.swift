JustasL/MapboxDirections.swift

add congestion levels

MapboxDirections/MBAttribute.swift
@@ -17,6 +17,8 @@ extension AttributeOptions: CustomStringConvertible {
             scope.update(with: .openStreetMapNodeIdentifier)
         case "speed":
             scope.update(with: .speed)
+        case "congestionLevel":
+            scope.update(with: .congestionLevel)
         default:
             return nil
         }
@@ -36,6 +38,9 @@ extension AttributeOptions: CustomStringConvertible {
         }
         if contains(.speed) {
             descriptions.append("speed")
+        }
+        if contains(.congestionLevel) {
+            descriptions.append("congestion")
         }
         return descriptions.joined(separator: ",")
     }

mapbox/mapbox-directions-swift

MapboxDirections/MBAttribute.swift
@@ -18,6 +18,8 @@ extension AttributeOptions: CustomStringConvertible {
                 attributeOptions.update(with: .openStreetMapNodeIdentifier)
             case "speed":
                 attributeOptions.update(with: .speed)
+            case "congestionLevel":
+                attributeOptions.update(with: .congestionLevel)
             case "":
                 continue
             default:
@@ -40,6 +42,9 @@ extension AttributeOptions: CustomStringConvertible {
         }
         if contains(.speed) {
             descriptions.append("speed")
+        }
+        if contains(.congestionLevel) {
+            descriptions.append("congestion")
         }
         return descriptions.joined(separator: ",")
     }