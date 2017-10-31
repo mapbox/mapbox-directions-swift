afacd2a6f0d7932388e7ae0068c374e655232e73
Bobby Sudekum <bobby@mapbox.com>
2017-10-31 14:56:20 -0700

    add imageBaseURL

--- MapboxDirections/MBVisualInstructionDetailComponent.swift
+++ MapboxDirections/MBVisualInstructionDetailComponent.swift
@@ -6,8 +6,12 @@ public class VisualInstructionDetailComponent: NSObject, NSSecureCoding {
     
     public let text: String?
     
+    public let imageBaseURL: URL?
+    
     internal init(json: JSONDictionary) {
         text = json["text"] as? String
+        
+        imageBaseURL = json["imageBaseURL"] as? URL
     }
     
     public required init?(coder decoder: NSCoder) {
@@ -15,11 +19,17 @@ public class VisualInstructionDetailComponent: NSObject, NSSecureCoding {
             return nil
         }
         self.text = text
+        
+        guard let imageBaseURL = decoder.decodeObject(of: NSURL.self, forKey: "imageBaseURL") as URL? else {
+            return nil
+        }
+        self.imageBaseURL = imageBaseURL
     }
     
     open static var supportsSecureCoding = true
     
     public func encode(with coder: NSCoder) {
         coder.encode(text, forKey: "text")
+        coder.encode(imageBaseURL, forKey: "imageBaseURL")
     }
 }
