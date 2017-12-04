dd78df862f1a86f4053aad681f641044b1e526b8
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2017-12-04 13:30:47 -0500

    Update product names (#208)

--- MapboxDirections/MBRouteStep.swift
+++ MapboxDirections/MBRouteStep.swift
@@ -689,7 +689,7 @@ open class RouteStep: NSObject, NSSecureCoding {
      
      The value of this property may be `nil`, for example when the maneuver type is `arrive`.
      
-     Using the [Mapbox iOS SDK](https://www.mapbox.com/ios-sdk/) or [Mapbox macOS SDK](https://github.com/mapbox/mapbox-gl-native/tree/master/platform/macos/), you can create an `MGLPolyline` object using these coordinates to display a portion of a route on an `MGLMapView`.
+     Using the [Mapbox Maps SDK for iOS](https://www.mapbox.com/ios-sdk/) or [Mapbox Maps SDK for macOS](https://github.com/mapbox/mapbox-gl-native/tree/master/platform/macos/), you can create an `MGLPolyline` object using these coordinates to display a portion of a route on an `MGLMapView`.
      */
     @objc open let coordinates: [CLLocationCoordinate2D]?
     
@@ -709,7 +709,7 @@ open class RouteStep: NSObject, NSSecureCoding {
      
      The array may be empty, for example when the maneuver type is `arrive`.
      
-     Using the [Mapbox iOS SDK](https://www.mapbox.com/ios-sdk/) or [Mapbox macOS SDK](https://github.com/mapbox/mapbox-gl-native/tree/master/platform/macos/), you can create an `MGLPolyline` object using these coordinates to display a portion of a route on an `MGLMapView`.
+     Using the [Mapbox Maps SDK for iOS](https://www.mapbox.com/ios-sdk/) or [Mapbox Maps SDK for macOS](https://github.com/mapbox/mapbox-gl-native/tree/master/platform/macos/), you can create an `MGLPolyline` object using these coordinates to display a portion of a route on an `MGLMapView`.
      
      - parameter coordinates: A pointer to a C array of `CLLocationCoordinate2D` instances. On output, this array contains all the vertices of the overlay.
      - returns: True if the step has coordinates and `coordinates` has been populated, or false if the step has no coordinates and `coordinates` has not been modified.
