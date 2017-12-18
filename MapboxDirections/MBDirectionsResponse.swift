79f78d65cd2b5f50e3ee4aa7e42ba9eba645ebbf
Jerrad Thramer <jerrad.thramer@mapbox.com>
2017-12-18 14:25:07 -0700

    Adding custom init that sets leg destination names properly now. All tests now pass.

--- MapboxDirections/MBDirectionsResponse.swift
+++ MapboxDirections/MBDirectionsResponse.swift
@@ -15,4 +15,41 @@ struct DirectionsResponse: Codable {
     let uuid: String?
     let routes: [Route]?
     let waypoints: [Waypoint]?
+ 
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let code = try container.decodeIfPresent(String.self, forKey: .code)
+        self.code = code
+        
+        let message = try container.decodeIfPresent(String.self, forKey: .message)
+        self.message = message
+        
+        let uuid = try container.decodeIfPresent(String.self, forKey: .uuid)
+        self.uuid = uuid
+        
+        //It's important that waypoints are decoded before routes
+        let waypoints = try container.decodeIfPresent([Waypoint].self, forKey: .waypoints)
+        self.waypoints = waypoints
+        
+        let rawRoutes = try container.decodeIfPresent([Route].self, forKey: .routes)
+        var routesWithDestinations: [Route]? = rawRoutes
+        if let destinations = waypoints?.dropFirst() {
+            routesWithDestinations = rawRoutes?.map({ (route) -> Route in
+                for (leg, destination) in zip(route.legs, destinations) {
+                    if leg.destination.name?.nonEmptyString == nil {
+                        leg.destination = destination
+                    }
+                }
+                return route
+            })
+        }
+        
+        let routesWithIdentifiers = routesWithDestinations?.map({ (route) -> Route in
+            route.routeIdentifier = uuid
+            return route
+        })
+        
+        self.routes = routesWithIdentifiers
+        
+    }
 }
