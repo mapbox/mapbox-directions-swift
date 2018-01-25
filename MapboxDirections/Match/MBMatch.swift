53054158f33a572d3bf350b73e536914010ad14e
Bobby Sudekum <bobby@mapbox.com>
2018-01-24 16:04:10 -0800

    Fixed but prob not fully correct

--- MapboxDirections/Match/MBMatch.swift
+++ MapboxDirections/Match/MBMatch.swift
@@ -7,8 +7,27 @@ open class Match: Route {
         super.init(routeOptions: matchOptions, legs: legs, distance: distance, expectedTravelTime: expectedTravelTime, coordinates: coordinates)
     }
     
-    @objc public init(json: [String: Any], tracePoints: [TracePoint], matchOptions: MatchOptions) {
-        super.init(json: json, waypoints: tracePoints, routeOptions: matchOptions)
+    convenience init(json: [String: Any], tracePoints: [TracePoint], matchOptions: MatchOptions) {
+        let legInfo = zip(zip(tracePoints.prefix(upTo: tracePoints.endIndex - 1), tracePoints.suffix(from: 1)),
+                          json["legs"] as? [JSONDictionary] ?? [])
+        let legs = legInfo.map { (endpoints, json) -> RouteLeg in
+            RouteLeg(json: json, source: endpoints.0, destination: endpoints.1, profileIdentifier: matchOptions.profileIdentifier)
+        }
+        
+        let distance = json["distance"] as! Double
+        let expectedTravelTime = json["duration"] as! Double
+        
+        var coordinates: [CLLocationCoordinate2D]?
+        switch json["geometry"] {
+        case let geometry as JSONDictionary:
+            coordinates = CLLocationCoordinate2D.coordinates(geoJSON: geometry)
+        case let geometry as String:
+            coordinates = decodePolyline(geometry, precision: 1e5)!
+        default:
+            coordinates = nil
+        }
+        
+        self.init(matchOptions: matchOptions, legs: legs, distance: distance, expectedTravelTime: expectedTravelTime, coordinates: coordinates)
     }
     
     @objc public required init?(coder decoder: NSCoder) {
