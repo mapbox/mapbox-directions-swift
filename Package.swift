1d59309a2d48669dbbcad164373794a85f67a4ca
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2019-04-09 14:57:47 +0200

    Build SPM flavor on CI

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "../Polyline", .branch("fred/spm4.2"))
+        .package(url: "https://github.com/raphaelmor/Polyline.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
