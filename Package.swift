318821c46d0ef4854a85dc687d4052cdb0939f37
Fredrik Karlsson <bjorn.fredrik.karlsson@gmail.com>
2019-08-20 09:49:10 +0200

    Specify supported platform in package manifest

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MapboxDirections",
+    platforms: [
+        .macOS(.v10_12), .iOS(.v10), .watchOS(.v4), .tvOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
