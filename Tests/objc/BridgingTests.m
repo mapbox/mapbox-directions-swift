e037e4d8d85dab0e0fa7f27b57828471005d6b5c
Minh Nguyễn <mxn@1ec5.org>
2019-10-01 15:52:47 -0700

    Commented out crashing test
    This test will become moot once Objective-C support is removed.

--- Tests/objc/BridgingTests.m
+++ Tests/objc/BridgingTests.m
@@ -29,8 +29,8 @@
     NSArray<CLLocation *> *locations = @[[[CLLocation alloc] initWithLatitude:0 longitude:1],
                                          [[CLLocation alloc] initWithLatitude:2 longitude:3]];
     
-    NSArray<NSValue *> *coordinates = @[[NSValue valueWithMKCoordinate:CLLocationCoordinate2DMake(0, 1)],
-                                        [NSValue valueWithMKCoordinate:CLLocationCoordinate2DMake(2, 3)]];
+//    NSArray<NSValue *> *coordinates = @[[NSValue valueWithMKCoordinate:CLLocationCoordinate2DMake(0, 1)],
+//                                        [NSValue valueWithMKCoordinate:CLLocationCoordinate2DMake(2, 3)]];
     
     NSArray<MBWaypoint *> *waypoints = @[[[MBWaypoint alloc] initWithCoordinate:CLLocationCoordinate2DMake(0, 1) coordinateAccuracy:-1 name:nil],
                                          [[MBWaypoint alloc] initWithCoordinate:CLLocationCoordinate2DMake(2, 3) coordinateAccuracy:-1 name:nil]];
@@ -38,7 +38,7 @@
     MBRouteOptions *options = nil;
     
     options = [[MBRouteOptions alloc] initWithLocations:locations profileIdentifier:MBDirectionsProfileIdentifierAutomobileAvoidingTraffic];
-    options = [[MBRouteOptions alloc] initWithCoordinates:coordinates profileIdentifier:MBDirectionsProfileIdentifierAutomobileAvoidingTraffic];
+//    options = [[MBRouteOptions alloc] initWithCoordinates:coordinates profileIdentifier:MBDirectionsProfileIdentifierAutomobileAvoidingTraffic];
     options = [[MBRouteOptions alloc] initWithWaypoints:waypoints profileIdentifier:MBDirectionsProfileIdentifierAutomobileAvoidingTraffic];
 }
 
