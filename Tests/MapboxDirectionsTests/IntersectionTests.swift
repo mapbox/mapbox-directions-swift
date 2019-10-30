42923910e1d94348f6640f06bc192dd122087198
Minh Nguyễn <mxn@1ec5.org>
2019-10-30 14:29:32 -0700

    Reverted to JSON-representable objects; implemented JSON comparison

--- Tests/MapboxDirectionsTests/IntersectionTests.swift
+++ Tests/MapboxDirectionsTests/IntersectionTests.swift
@@ -2,108 +2,70 @@ import XCTest
 @testable import MapboxDirections
 
 class IntersectionTests: XCTestCase {
-    
-    let intersectionJSON =
-    """
-    [
-      {
-        "location" : [
-          13.426579,
-          52.508068
-        ],
-        "in" : -1,
-        "classes" : [
-          "toll",
-          "restricted"
-        ],
-        "usableApproachLanes" : null,
-        "bearings" : [
-          80
-        ],
-        "entry" : [
-            true
-        ],
-        "out" : 0,
-        "approachLanes" : null
-      },
-      {
-        "location" : [
-          13.426688,
-          52.508022
-        ],
-        "in" : 2,
-        "usableApproachLanes" : null,
-        "bearings" : [
-          30,
-          120,
-          300
-        ],
-        "entry" : [
-            false,
-            true,
-            true
-        ],
-        "out" : 1,
-        "approachLanes" : null
-      }
-    ]
-    """
-    
-    let pass = """
-    [
-      {
-        \"location\" : [
-          13.426579,
-          52.508068000000002
-        ],
-        \"in\" : -1,
-        \"classes\" : [
-          \"toll\",
-          \"restricted\"
-        ],
-        \"bearings\" : [
-          80
-        ],
-        \"entry\" : [
-          true
-        ],
-        \"lanes\" : null,
-        \"out\" : 0
-      },
-      {
-        \"entry\" : [
-          false,
-          true,
-          true
-        ],
-        \"in\" : 2,
-        \"out\" : 1,
-        \"lanes\" : null,
-        \"location\" : [
-          13.426688,
-          52.508021999999997
-        ],
-        \"bearings\" : [
-          30,
-          120,
-          300
-        ]
-      }
-    ]
-    """
-    
     func testCoding() {
+        let intersectionsJSON: [[String: Any?]] = [
+            [
+                "location": [13.426579, 52.508068],
+                "in": -1,
+                "classes": ["toll", "restricted"],
+                "usableApproachLanes": nil,
+                "bearings": [80],
+                "entry": [true],
+                "out": 0,
+                "approachLanes": nil,
+            ],
+            [
+                "location": [13.426688, 52.508022],
+                "in": 2,
+                "usableApproachLanes": nil,
+                "bearings": [30, 120, 300],
+                "entry": [false, true, true],
+                "out": 1,
+                "approachLanes": nil,
+            ],
+        ]
+        let intersectionsData = try! JSONSerialization.data(withJSONObject: intersectionsJSON, options: [])
+        var intersections: [Intersection]?
+        XCTAssertNoThrow(intersections = try JSONDecoder().decode([Intersection].self, from: intersectionsData))
+        XCTAssertEqual(intersections?.count, 2)
         
-        let intersections = try! JSONDecoder().decode([Intersection].self, from: intersectionJSON.data(using: .utf8)!)
-        let intersection = intersections.first!
+        if let intersection = intersections?.first {
+            XCTAssert(intersection.outletRoadClasses == [.toll, .restricted])
+            XCTAssert(intersection.headings == [80.0])
+            XCTAssert(intersection.location == CLLocationCoordinate2D(latitude: 52.508068, longitude: 13.426579))
+        }
         
-        XCTAssert(intersection.outletRoadClasses == [.toll, .restricted])
-        XCTAssert(intersection.headings == [80.0])
-        XCTAssert(intersection.location == CLLocationCoordinate2D(latitude:  52.508068, longitude: 13.426579) )
+        intersections = [
+            Intersection(location: CLLocationCoordinate2D(latitude: 52.508068, longitude: 13.426579),
+                         headings: [80.0],
+                         approachIndex: -1,
+                         outletIndex: 0,
+                         outletIndexes: IndexSet([0]),
+                         approachLanes: nil,
+                         usableApproachLanes: nil,
+                         outletRoadClasses: [.toll, .restricted]),
+            Intersection(location: CLLocationCoordinate2D(latitude: 52.508022, longitude: 13.426688),
+                         headings: [30.0, 120.0, 300.0],
+                         approachIndex: 2,
+                         outletIndex: 1,
+                         outletIndexes: IndexSet([1, 2]),
+                         approachLanes: nil,
+                         usableApproachLanes: nil,
+                         outletRoadClasses: nil)
+        ]
+        
         let encoder = JSONEncoder()
-        encoder.outputFormatting = [.prettyPrinted]
-        let encoded = String(data: try! encoder.encode(intersections), encoding: .utf8)
+        var encodedData: Data?
+        XCTAssertNoThrow(encodedData = try encoder.encode(intersections))
+        XCTAssertNotNil(encodedData)
         
-        XCTAssert(encoded == pass)
+        if let encodedData = encodedData {
+            var encodedIntersectionsJSON: [[String: Any?]]?
+            XCTAssertNoThrow(encodedIntersectionsJSON = try JSONSerialization.jsonObject(with: encodedData, options: []) as? [[String: Any?]])
+            XCTAssertNotNil(encodedIntersectionsJSON)
+            if let encodedIntersectionsJSON = encodedIntersectionsJSON {
+                XCTAssert(JSONSerialization.objectsAreEqual(intersectionsJSON, encodedIntersectionsJSON, approximate: true))
+            }
+        }
     }
 }
--- Tests/MapboxDirectionsTests/VisualInstructionComponentTests.swift
+++ Tests/MapboxDirectionsTests/VisualInstructionComponentTests.swift
@@ -1,17 +1,25 @@
 import XCTest
 @testable import MapboxDirections
 
-let componentJSON = """
-{
-"type": "text",
-"text": "Take a hike",
-"imageBaseURL": "",
-}
-"""
 class VisualInstructionComponentTests: XCTestCase {
     func testJSONInitialization() {
-        let component = try! JSONDecoder().decode(Component.self, from: componentJSON.data(using:.utf8)!).component as! VisualInstructionComponent
-        XCTAssertEqual(component.text, "Take a hike")
-        XCTAssertNil(component.imageURL)
+        let componentJSON = [
+            "type": "text",
+            "text": "Take a hike",
+            "imageBaseURL": "",
+        ]
+        let componentData = try! JSONSerialization.data(withJSONObject: componentJSON, options: [])
+        var component: Component?
+        XCTAssertNoThrow(component = try JSONDecoder().decode(ComponentSerializer.self, from: componentData).component)
+        XCTAssertNotNil(component)
+        if let component = component {
+            switch component {
+            case .lane(_):
+                XCTFail("Text component should not be interpreted as lane component.")
+            case let .visual(component):
+                XCTAssertEqual(component.text, "Take a hike")
+                XCTAssertNil(component.imageURL)
+            }
+        }
     }
 }
