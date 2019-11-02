1bbecb5a19a57d8cb30b2acbd1de4fb2bedcb25b
Minh Nguyễn <mxn@1ec5.org>
2019-11-02 11:06:52 -0700

    Avoid ExpressibleByStringLiteral for enumeration types

--- Sources/MapboxDirections/DirectionsProfileIdentifier.swift
+++ Sources/MapboxDirections/DirectionsProfileIdentifier.swift
@@ -8,14 +8,7 @@ The returned directions are appropriate for driving or riding a car, truck, or m
 
 This profile prioritizes fast routes by preferring high-speed roads like highways. A driving route may use a ferry where necessary.
 */
-public struct DirectionsProfileIdentifier: Codable, Hashable, RawRepresentable, ExpressibleByStringLiteral {
-    public typealias StringLiteralType = String
-    public typealias RawValue = String
-    
-    public init(stringLiteral value: StringLiteralType) {
-        self.init(rawValue: value)
-    }
-    
+public struct DirectionsProfileIdentifier: Codable, Hashable, RawRepresentable {
     public init(rawValue: String) {
         self.rawValue = rawValue
     }
@@ -27,7 +20,7 @@ public struct DirectionsProfileIdentifier: Codable, Hashable, RawRepresentable, 
     
     This profile prioritizes fast routes by preferring high-speed roads like highways. A driving route may use a ferry where necessary.
     */
-    public static let automobile: DirectionsProfileIdentifier = "mapbox/driving"
+    public static let automobile: DirectionsProfileIdentifier = .init(rawValue: "mapbox/driving")
     
     /**
     The returned directions are appropriate for driving or riding a car, truck, or motorcycle.
@@ -36,21 +29,21 @@ public struct DirectionsProfileIdentifier: Codable, Hashable, RawRepresentable, 
     
     Traffic data is available in [a number of countries and territories worldwide](https://docs.mapbox.com/help/how-mapbox-works/directions/#traffic-data). Where traffic data is unavailable, this profile prefers high-speed roads like highways, similar to `DirectionsProfileIdentifier.Automobile`.
     */
-    public static let automobileAvoidingTraffic: DirectionsProfileIdentifier = "mapbox/driving-traffic"
+    public static let automobileAvoidingTraffic: DirectionsProfileIdentifier = .init(rawValue: "mapbox/driving-traffic")
     
     /**
     The returned directions are appropriate for riding a bicycle.
     
     This profile prioritizes short, safe routes by avoiding highways and preferring cycling infrastructure, such as bike lanes on surface streets. A cycling route may, where necessary, use other modes of transportation, such as ferries or trains, or require dismounting the bicycle for a distance.
     */
-    public static let cycling: DirectionsProfileIdentifier = "mapbox/cycling"
+    public static let cycling: DirectionsProfileIdentifier = .init(rawValue: "mapbox/cycling")
     
     /**
     The returned directions are appropriate for walking or hiking.
     
     This profile prioritizes short routes, making use of sidewalks and trails where available. A walking route may use other modes of transportation, such as ferries or trains, where necessary.
     */
-    public static let walking: DirectionsProfileIdentifier = "mapbox/walking"
+    public static let walking: DirectionsProfileIdentifier = .init(rawValue: "mapbox/walking")
 }
 
 @available(*, deprecated, renamed: "DirectionsPriority")
