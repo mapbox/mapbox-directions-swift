81504bbb5b929c86ea17e16b89be55777ade822d
Minh Nguyễn <mxn@1ec5.org>
2019-11-22 21:34:05 -0800

    Fixed Route documentation
    The Route class documentation comment was attached to the private CodingKeys enumeration for some reason. Also removed an extraneous import statement.

--- Sources/MapboxDirections/Route.swift
+++ Sources/MapboxDirections/Route.swift
@@ -1,15 +1,13 @@
-import Polyline
-
 /**
  A `Route` object defines a single route that the user can follow to visit a series of waypoints in order. The route object includes information about the route, such as its distance and expected travel time. Depending on the criteria used to calculate the route, the route object may also include detailed turn-by-turn instructions.
  
  Typically, you do not create instances of this class directly. Instead, you receive route objects when you request directions using the `Directions.calculate(_:completionHandler:)` method. However, if you use the `Directions.url(forCalculating:)` method instead, you can pass the results of the HTTP request into this class’s initializer.
  */
-private enum CodingKeys: String, CodingKey {
-    case routeOptions
-}
-
 open class Route: DirectionsResult {
+    private enum CodingKeys: String, CodingKey {
+        case routeOptions
+    }
+    
     public required init(from decoder: Decoder) throws {
         if let matchOptions = decoder.userInfo[.options] as? MatchOptions {
             routeOptions = RouteOptions(matchOptions: matchOptions)
