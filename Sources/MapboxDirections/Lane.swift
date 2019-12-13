56bf20116b02e45058f0a76386ce534900be7e60
Jerrad Thramer <Jerrad.Thramer@mapbox.com>
2019-12-13 15:44:09 -0700

    Going to need to take Lane public.

--- Sources/MapboxDirections/Lane.swift
+++ Sources/MapboxDirections/Lane.swift
@@ -3,15 +3,15 @@ import Foundation
 /**
  A lane on the road approaching an intersection.
  */
-struct Lane: Equatable {
+public struct Lane: Equatable {
     /**
      The lane indications specifying the maneuvers that may be executed from the lane.
      */
-    let indications: LaneIndication
+    public let indications: LaneIndication
     
-    var isValid: Bool
+    public var isValid: Bool
     
-    init(indications: LaneIndication, valid: Bool = false) {
+    public init(indications: LaneIndication, valid: Bool = false) {
         self.indications = indications
         self.isValid = valid
     }
@@ -23,13 +23,13 @@ extension Lane: Codable {
         case valid
     }
     
-    func encode(to encoder: Encoder) throws {
+    public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(indications, forKey: .indications)
         try container.encode(isValid, forKey: .valid)
     }
     
-    init(from decoder: Decoder) throws {
+    public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         indications = try container.decode(LaneIndication.self, forKey: .indications)
         isValid = try container.decode(Bool.self, forKey: .valid)
