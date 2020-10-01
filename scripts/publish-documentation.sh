81f230cf09760b8b86d9a696f8ca3ed1f3d63710
Katy DeCorah <decorah@mapbox.com>
2020-10-01 10:02:56 -0400

    Fix folder name

--- scripts/publish-documentation.sh
+++ scripts/publish-documentation.sh
@@ -10,12 +10,13 @@ trap finish EXIT
 
 OUTPUT="/tmp/`uuidgen`"
 RELEASE_BRANCH=${1:-master}
+VERSION=${1}
+FOLDER=${VERSION:1} # remove first character from VERSION (v)
 
 step "Updating mapbox-directions-swift repository…"
 git fetch --depth=1 --prune
 git fetch --tags
 git checkout $RELEASE_BRANCH
-VERSION=${1}
 
 git checkout $VERSION
 
@@ -28,15 +29,15 @@ gem install jazzy
 step "Generating new docs for ${VERSION}…"
 OUTPUT=${OUTPUT} scripts/document.sh
 
-step "Moving new docs folder to ./$VERSION"
-rm -rf "./$VERSION"
-mkdir -p "./$VERSION"
-mv -v $OUTPUT/* "./$VERSION"
+step "Moving new docs folder to ./$FOLDER"
+rm -rf "./$FOLDER"
+mkdir -p "./$FOLDER"
+mv -v $OUTPUT/* "./$FOLDER"
 
 step "Switching branch to publisher-production"
 git checkout origin/publisher-production
 step "Committing API docs for $VERSION"
-git add "./$VERSION"
+git add "./$FOLDER"
 git commit -m "[directions] Add Mapbox Directions for Swift API docs for $VERSION [ci skip]" --no-verify
 
 step "Finished updating documentation"
