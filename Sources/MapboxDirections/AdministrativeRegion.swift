36fa12d431760ce151fb8e5bc3a5837eff27056b
udumft <viktor.kononov@mapbox.com>
2020-11-03 16:38:31 +0300

    vk-477-admin-decode: made `Intersection.administrativeRegionIndex` internal, added `regionCode` and it's decoding, added `RouteStep.administrativeRegionIndicesByIntersection`.

--- Sources/MapboxDirections/AdministrativeRegion.swift
+++ Sources/MapboxDirections/AdministrativeRegion.swift
@@ -11,25 +11,11 @@ public struct AdministrativeRegion: Codable, Equatable {
         case countryCode = "iso_3166_1"
     }
 
-    public var countryCodeAlpha3: String?
+    public var countryCodeAlpha3: String
     public var countryCode: String
 
     public init(countryCode: String, countryCodeAlpha3: String) {
         self.countryCode = countryCode
         self.countryCodeAlpha3 = countryCodeAlpha3
-    }
-    
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        countryCode = try container.decode(String.self, forKey: .countryCode)
-        countryCodeAlpha3 = try container.decodeIfPresent(String.self, forKey: .countryCodeAlpha3)
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try container.encode(countryCode, forKey: .countryCode)
-        try container.encodeIfPresent(countryCodeAlpha3, forKey: .countryCodeAlpha3)
     }
 }
