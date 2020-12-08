0b81298862ea94a88e86c59c2c3f3e47d5e375b8
udumft <viktor.kononov@mapbox.com>
2020-12-08 16:25:33 +0300

    vk-458-road-classification: CHANGELOG corrected. Updated code docs for MapboxStreetClass

--- Sources/MapboxDirections/MapboxStreetClass.swift
+++ Sources/MapboxDirections/MapboxStreetClass.swift
@@ -1,65 +1,68 @@
 
 import Foundation
 
+/// 'Mapbox Streets V8' compliant street class definition
+///
+/// For detailed info on road classes, see [the documentation](https://docs.mapbox.com/vector-tiles/reference/mapbox-streets-v8/#road)
 public enum MapboxStreetClass: String, Codable {
     /// High-speed, grade-separated highways
-    case Motorway = "motorway"
+    case motorway = "motorway"
     /// Link roads/lanes/ramps connecting to motorways
-    case MotorwayLink = "motorway_link"
+    case motorwayLink = "motorway_link"
     /// Important roads that are not motorways.
-    case Trunk = "trunk"
+    case trunk = "trunk"
     /// Link roads/lanes/ramps connecting to trunk roads
-    case TrunkLink = "trunk_link"
+    case trunkLink = "trunk_link"
     /// A major highway linking large towns.
-    case Primary = "primary"
+    case primary = "primary"
     /// Link roads/lanes connecting to primary roads
-    case PrimaryLink = "primary_link"
+    case primaryLink = "primary_link"
     /// A highway linking large towns.
-    case Secondary = "secondary"
+    case secondary = "secondary"
     /// Link roads/lanes connecting to secondary roads
-    case SecondaryLink = "secondary_link"
+    case secondaryLink = "secondary_link"
     /// A road linking small settlements, or the local centres of a large town or city.
-    case Tertiary = "tertiary"
+    case tertiary = "tertiary"
     /// Link roads/lanes connecting to tertiary roads
-    case TertiaryLink = "tertiary_link"
+    case tertiaryLink = "tertiary_link"
     /// Standard unclassified, residential, road, and living_street road types
-    case Street = "street"
+    case street = "street"
     /// Streets that may have limited or no access for motor vehicles.
-    case StreetLimited = "street_limited"
+    case streetLimited = "street_limited"
     /// Includes pedestrian streets, plazas, and public transportation platforms.
-    case Pedestrian = "pedestrian"
+    case pedestrian = "pedestrian"
     /// Includes motor roads under construction (but not service roads, paths, etc.
-    case Construction = "construction"
+    case construction = "construction"
     /// Roads mostly for agricultural and forestry use etc.
-    case Track = "track"
+    case track = "track"
     /// Access roads, alleys, agricultural tracks, and other services roads. Also includes parking lot aisles, public & private driveways.
-    case Service = "service"
+    case service = "service"
     /// Those that serves automobiles and no or unspecified automobile service.
-    case Ferry = "ferry"
+    case ferry = "ferry"
     /// Foot paths, cycle paths, ski trails.
-    case Path = "path"
+    case path = "path"
     /// Railways, including mainline, commuter rail, and rapid transit.
-    case MajorRail = "major_rail"
+    case majorRail = "major_rail"
     /// Includes light rail & tram lines.
-    case MinorRail = "minor_rail"
+    case minorRail = "minor_rail"
     /// Yard and service railways.
-    case ServiceRail = "service_rail"
+    case serviceRail = "service_rail"
     /// Ski lifts, gondolas, and other types of aerialway.
-    case Aerialway = "aerialway"
+    case aerialway = "aerialway"
     /// The approximate centerline of a golf course hole
-    case Golf = "golf"
+    case golf = "golf"
     /// Circular continuous-flow intersection
-    case Roundabout = "roundabout"
+    case roundabout = "roundabout"
     /// Smaller variation of a roundabout with no center island or obstacle
-    case MiniRoundabout = "mini_roundabout"
-    /// (point Widened section at the end of a cul-de-sac for turning around a vehicle
-    case TurningCircle = "turning_circle"
-    /// (point Similar to a turning circle but with an island or other obstruction at the centerpoint
-    case TurningLoop = "turning_loop"
-    /// (point Lights or other signal controlling traffic flow at an intersection
-    case TrafficSignals = "traffic_signals"
-    /// (point A point indication for road junctions
-    case Junction = "junction"
-    /// (point Indicating the class and type of roads meeting at an intersection. Intersections are only available in Japan
-    case Intersection = "intersection"
+    case miniRoundabout = "mini_roundabout"
+    /// Widened section at the end of a cul-de-sac for turning around a vehicle. (point) 
+    case turningCircle = "turning_circle"
+    /// Similar to a turning circle but with an island or other obstruction at the centerpoint. (point)
+    case turningLoop = "turning_loop"
+    /// Lights or other signal controlling traffic flow at an intersection. (point)
+    case trafficSignals = "traffic_signals"
+    /// A point indication for road junctions. (point)
+    case junction = "junction"
+    /// Indicating the class and type of roads meeting at an intersection. Intersections are only available in Japan. (point)
+    case intersection = "intersection"
 }
