7a5bdd732e378e5167b47e89eb5ae05e1610177d
Nikolay Volosatov <mikalai.valasatau@mapbox.com>
2021-09-23 10:12:50 +0300

    Fix segment indices (#587)
    * Update tests to match real Directions response
    
    * Fix segmentRangesByStep for arrival
    
    * Add no-shape step to the test
    
    * Update CHANGELOG

--- Tests/MapboxDirectionsTests/RouteLegTests.swift
+++ Tests/MapboxDirectionsTests/RouteLegTests.swift
@@ -9,6 +9,7 @@ class RouteLegTests: XCTestCase {
             LocationCoordinate2D(latitude: 0, longitude: 0),
             LocationCoordinate2D(latitude: 1, longitude: 1),
         ])
+        let noShapeStep = RouteStep(transportType: .automobile, maneuverLocation: LocationCoordinate2D(latitude: 1, longitude: 1), maneuverType: .continue, instructions: "Continue", drivingSide: .right, distance: 0, expectedTravelTime: 0)
         let turnStep = RouteStep(transportType: .automobile, maneuverLocation: LocationCoordinate2D(latitude: 1, longitude: 1), maneuverType: .turn, maneuverDirection: .left, instructions: "Turn left at Albuquerque", drivingSide: .right, distance: 10, expectedTravelTime: 10)
         turnStep.shape = LineString([
             LocationCoordinate2D(latitude: 1, longitude: 1),
@@ -18,13 +19,17 @@ class RouteLegTests: XCTestCase {
         ])
         let typicalTravelTime = 10.0
         let arrivalStep = RouteStep(transportType: .automobile, maneuverLocation: LocationCoordinate2D(latitude: 4, longitude: 4), maneuverType: .arrive, instructions: "Arrive at Elmer’s House", drivingSide: .right, distance: 0, expectedTravelTime: 0)
-        let leg = RouteLeg(steps: [departureStep, turnStep, arrivalStep], name: "", distance: 10, expectedTravelTime: 10, typicalTravelTime: typicalTravelTime, profileIdentifier: .automobile)
+        arrivalStep.shape = LineString([
+            LocationCoordinate2D(latitude: 4, longitude: 4),
+            LocationCoordinate2D(latitude: 4, longitude: 4),
+        ])
+        let leg = RouteLeg(steps: [departureStep, noShapeStep, turnStep, arrivalStep], name: "", distance: 10, expectedTravelTime: 10, typicalTravelTime: typicalTravelTime, profileIdentifier: .automobile)
         leg.segmentDistances = [
             10,
             10, 20, 30,
         ]
         XCTAssertEqual(leg.segmentRangesByStep.count, leg.steps.count)
-        XCTAssertEqual(leg.segmentRangesByStep, [0..<1, 1..<4, 4..<4])
+        XCTAssertEqual(leg.segmentRangesByStep, [0..<1, 1..<1, 1..<4, 4..<4])
         XCTAssertEqual(leg.segmentRangesByStep.last?.upperBound, leg.segmentDistances?.count)
         XCTAssertEqual(leg.typicalTravelTime, typicalTravelTime)
     }
