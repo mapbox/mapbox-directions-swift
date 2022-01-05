9a83bacd2bda5d53ac39a4735eb31b80229c5020
Jill Cardamon <jillian.cardamon@mapbox.com>
2022-01-05 15:00:31 -0800

    Incorporate feedback, refactor to account for map matching.

--- Sources/MapboxDirectionsCLI/CodingOperation.swift
+++ Sources/MapboxDirectionsCLI/CodingOperation.swift
@@ -18,7 +18,9 @@ class CodingOperation<ResponceType : Codable, OptionsType : DirectionsOptions > 
         return try encoder.encode(result)
     }
     
-    private func processOutput(_ data: Data, routeResponse: RouteResponse?) throws {
+    private func processOutput(_ data: Data,
+                               routeResponse: RouteResponse? = nil,
+                               matchResponse: MapMatchingResponse? = nil) throws {
         var outputText: String = ""
         
         switch options.outputFormat {
@@ -31,37 +33,30 @@ class CodingOperation<ResponceType : Codable, OptionsType : DirectionsOptions > 
             }
             
         case .gpx:
-            #if !os(Linux)
             var gpxText: String = String("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
             gpxText.append("\n<gpx xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.topografix.com/GPX/1/1\" xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\" version=\"1.1\">")
             
-            guard let routeResponse = routeResponse,
-                  let routes = routeResponse.routes else { return }
-            
-            let timeInterval: TimeInterval = 1
-            let dateFormatter = ISO8601DateFormatter()
-            dateFormatter.formatOptions = .withInternetDateTime
-            var time = Date()
-            
-            routes.forEach { route in
-                let coordinates = interpolate(route: route, timeInterval: timeInterval)
-                for coord in coordinates {
-                    guard let lat = coord?.latitude, let lon = coord?.longitude else { continue }
-                    gpxText.append("\n<wpt lat=\"\(lat)\" lon=\"\(lon)\">")
-                    gpxText.append("\n\t<time> \(dateFormatter.string(from: time)) </time>")
-                    gpxText.append("\n</wpt>")
-                    time.addTimeInterval(timeInterval)
+            if let routeResponse = routeResponse,
+               let routes = routeResponse.routes {
+                routes.forEach { route in
+                    let text = populateGPX(route)
+                    gpxText.append(text)
+                    if routes.count > 1 {
+                        gpxText.append("<!--Moving to next route-->")
+                    }
                 }
-                if routes.count > 1 {
-                    gpxText.append("<!--Moving to next route-->")
+            } else if let matchResponse = matchResponse,
+                      let matches = matchResponse.matches {
+                matches.forEach { match in
+                    let text = populateGPX(nil, match)
+                    gpxText.append(text)
+                    if matches.count > 1 {
+                        gpxText.append("<!--Moving to next match-->")
+                    }
                 }
             }
             gpxText.append("\n</gpx>")
             outputText = gpxText
-            #else
-            print("GPX output requires use of CoreLocation, a framework that is not available on Linux.")
-            exit(1)
-            #endif
         }
         
         if let outputPath = options.outputPath {
@@ -73,20 +68,53 @@ class CodingOperation<ResponceType : Codable, OptionsType : DirectionsOptions > 
         }
     }
     
-    private func interpolate(route: Route, timeInterval: TimeInterval) -> [LocationCoordinate2D?] {
-        guard route.expectedTravelTime > 0, let polyline = route.shape,
-              let firstCoordinate = polyline.coordinates.first else { return [] }
+    private func populateGPX(_ route: Route? = nil, _ match: Match? = nil) -> String {
+        let timeInterval: TimeInterval = 1
+        let dateFormatter = ISO8601DateFormatter()
+        dateFormatter.formatOptions = .withInternetDateTime
+        var time = Date()
+        var text: String = ""
+        var coordinates: [LocationCoordinate2D?] = []
+        
+        if route != nil {
+            guard let route = route else { return "" }
+            coordinates = interpolate(shape: route.shape,
+                                      expectedTravelTime: route.expectedTravelTime,
+                                      distance: route.distance,
+                                      timeInterval: timeInterval)
+        } else if match != nil {
+            guard let match = match else { return "" }
+            coordinates = interpolate(shape: match.shape,
+                                      expectedTravelTime: match.expectedTravelTime,
+                                      distance: match.distance,
+                                     timeInterval: timeInterval)
+        }
+        
+        for coord in coordinates {
+            guard let lat = coord?.latitude, let lon = coord?.longitude else { continue }
+            text.append("\n<wpt lat=\"\(lat)\" lon=\"\(lon)\">")
+            text.append("\n\t<time> \(dateFormatter.string(from: time)) </time>")
+            text.append("\n</wpt>")
+            time.addTimeInterval(timeInterval)
+        }
+        return text
+    }
+    
+    private func interpolate(shape: LineString?, expectedTravelTime: TimeInterval, distance: LocationDistance, timeInterval: TimeInterval) -> [LocationCoordinate2D?] {
+        guard expectedTravelTime > 0, let polyline = shape,
+              let firstCoordinate = polyline.coordinates.first,
+              let lastCoordinate = polyline.coordinates.last else { return [] }
         
         var distanceAway: LocationDistance = 0
-        let distancePerTick = route.distance/route.expectedTravelTime
+        let distancePerTick = distance/expectedTravelTime
         var interpolatedCoordinates = [firstCoordinate]
-        while distanceAway <= route.distance {
+        while distanceAway <= distance {
             if let nextCoordinate = polyline.coordinateFromStart(distance: distanceAway) {
                 interpolatedCoordinates.append(nextCoordinate)
             }
             distanceAway += distancePerTick * timeInterval
         }
-        interpolatedCoordinates.append(polyline.coordinates.last)
+        interpolatedCoordinates.append(lastCoordinate)
         return interpolatedCoordinates
     }
     
@@ -109,12 +137,17 @@ class CodingOperation<ResponceType : Codable, OptionsType : DirectionsOptions > 
                             .credentials: BogusCredentials]
         
         var routeResponse: RouteResponse?
+        var matchResponse: MapMatchingResponse?
         if options.outputFormat == .gpx {
             let gpxData = try String(contentsOfFile: options.inputPath).data(using: .utf8)!
-            routeResponse = try! decoder.decode(RouteResponse.self, from: gpxData)
+            do {
+                routeResponse = try decoder.decode(RouteResponse.self, from: gpxData)
+            } catch {
+                matchResponse = try decoder.decode(MapMatchingResponse.self, from: gpxData)
+            }
         }
         let data = try processResponse(decoder, type: ResponceType.self, from: input)
         
-        try processOutput(data, routeResponse: routeResponse)
+        try processOutput(data, routeResponse: routeResponse, matchResponse: matchResponse)
     }
 }
