27b10cca6be014f417cd5558f0dfb6a47ec8e35f
Minh Nguyễn <mxn@1ec5.org>
2022-05-04 17:19:03 -0700

    Restored compatibility initializer for RestStop

--- Sources/MapboxDirections/RestStop.swift
+++ Sources/MapboxDirections/RestStop.swift
@@ -26,12 +26,22 @@ public struct RestStop: Codable, Equatable {
     }
     
     /**
-     Initializes a rest stop with the given kind and name.
+     Initializes an unnamed rest stop of a certain kind.
+     
+     - parameter type: The kind of rest stop.
+     */
+    public init(type: StopType) {
+        self.type = type
+        self.name = nil
+    }
+    
+    /**
+     Initializes an optionally named rest stop of a certain kind.
      
      - parameter type: The kind of rest stop.
      - parameter name: The name of the rest stop.
      */
-    public init(type: StopType, name: String? = nil) {
+    public init(type: StopType, name: String?) {
         self.type = type
         self.name = name
     }
