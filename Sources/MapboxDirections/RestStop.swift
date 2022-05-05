67c2344ac2d551122d3b68f107f66a5535c0c3cd
udumft <viktor.kononov@mapbox.com>
2022-05-05 16:21:37 +0300

    vk-637-json-roundtrip: added fixed RestStop coding

--- Sources/MapboxDirections/RestStop.swift
+++ Sources/MapboxDirections/RestStop.swift
@@ -52,6 +52,7 @@ public struct RestStop: Codable, Equatable, ForeignMemberContainer {
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         type = try container.decode(StopType.self, forKey: .type)
+        name = try container.decodeIfPresent(String.self, forKey: .name)
         
         try decodeForeignMembers(notKeyedBy: CodingKeys.self, with: decoder)
     }
@@ -59,11 +60,12 @@ public struct RestStop: Codable, Equatable, ForeignMemberContainer {
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(type, forKey: .type)
+        try container.encodeIfPresent(name, forKey: .name)
         
         try encodeForeignMembers(notKeyedBy: CodingKeys.self, to: encoder)
     }
     
     public static func == (lhs: Self, rhs: Self) -> Bool {
-        return lhs.type == rhs.type
+        return lhs.type == rhs.type && lhs.name == rhs.name
     }
 }
