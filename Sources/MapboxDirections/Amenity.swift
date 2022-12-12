17635d53934dc3ef3ad4393b40805befc65a5a40
Maxim Makhun <maxim.makhun@mapbox.com>
2022-12-12 11:20:05 -0800

    Update.

--- Sources/MapboxDirections/Amenity.swift
+++ Sources/MapboxDirections/Amenity.swift
@@ -20,6 +20,12 @@ public struct Amenity: Codable, Equatable {
      */
     public let type: AmenityType
     
+    private enum CodingKeys: String, CodingKey {
+        case type
+        case name
+        case brand
+    }
+    
     /**
      Initializes an `Amenity`.
      
@@ -31,6 +37,20 @@ public struct Amenity: Codable, Equatable {
         self.type = type
         self.name = name
         self.brand = brand
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        type = try container.decode(AmenityType.self, forKey: .type)
+        name = try container.decodeIfPresent(String.self, forKey: .name)
+        brand = try container.decodeIfPresent(String.self, forKey: .brand)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(type, forKey: .type)
+        try container.encode(name, forKey: .name)
+        try container.encode(brand, forKey: .brand)
     }
     
     public static func == (lhs: Self, rhs: Self) -> Bool {
