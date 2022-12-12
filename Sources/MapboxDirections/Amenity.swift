44f7f471fa8f1947b95ef4867f53441794a93018
Maxim Makhun <maxim.makhun@mapbox.com>
2022-12-12 10:24:25 -0800

    Update.

--- Sources/MapboxDirections/Amenity.swift
+++ Sources/MapboxDirections/Amenity.swift
@@ -8,17 +8,30 @@ public struct Amenity: Codable, Equatable {
     /**
      Name of the amenity, if available.
      */
-    var name: String?
+    public let name: String?
     
     /**
      Brand of the amenity, if available.
      */
-    var brand: String?
+    public let brand: String?
     
     /**
      Type of the amenity.
      */
-    var type: AmenityType
+    public let type: AmenityType
+    
+    /**
+     Initializes an `Amenity`.
+     
+     - parameter type: Type of the amenity.
+     - parameter name: Name of the amenity.
+     - parameter brand: Brand of the amenity.
+     */
+    public init(type: AmenityType, name: String? = nil, brand: String? = nil) {
+        self.type = type
+        self.name = name
+        self.brand = brand
+    }
     
     public static func == (lhs: Self, rhs: Self) -> Bool {
         return lhs.name == rhs.name &&
