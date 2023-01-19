a045f6ea128420163a1adbe09017f8d29fde4d84
Aliaksandr Bialiauski <413986+S2Ler@users.noreply.github.com>
2023-01-19 07:51:57 +0300

    Add CI integration with Mapbox Navigation SDK

--- IntegrationWithNavigationSDK/MapboxNavigationSwiftUI/Directions+asyncAwait.swift
+++ IntegrationWithNavigationSDK/MapboxNavigationSwiftUI/Directions+asyncAwait.swift
@@ -2,9 +2,9 @@ import MapboxCoreNavigation
 import MapboxDirections
 
 extension MapboxRoutingProvider {
-    func calculate(options: RouteOptions) async throws -> IndexedRouteResponse {
+    func calculate(options: RouteOptions) async throws -> RouteResponse {
         try await withCheckedThrowingContinuation { continuation in
-            _ = calculateRoutes(options: options) { result in
+            _ = calculateRoutes(options: options) { _, result in
                 switch result {
                 case .success(let response):
                     continuation.resume(returning: response)
