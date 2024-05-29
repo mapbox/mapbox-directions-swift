e5b0945a3f1e0bc362cf399bf3cfc20596d89cf0
Alex Azarov <alex@azarov.by>
2024-05-29 18:18:09 +0200

    Revert Package.swift

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .package(url: "https://github.com/raphaelmor/Polyline.git", from: "5.0.2"),
         .package(name: "Turf", url: "https://github.com/mapbox/turf-swift.git", "2.8.0"..<"2.9.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
-        .package(url: "https://github.com/AliSoftware/OHHTTPStubs", revision: "c582400a38590a3dabb4353416d9d46cb7278d06") // Xcode 14.3+ support
+        .package(url: "https://github.com/AliSoftware/OHHTTPStubs", from: "9.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
